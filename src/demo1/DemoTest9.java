155892df576c29ce69ae3971d45a72d71f3c1b45
RAFEEF <RAFEEF@10.0.0.65>
2025-01-31 16:08:03 -0500

    implemented subtract()

--- src/demo1/DemoTest9.java
+++ src/demo1/DemoTest9.java
@@ -2,7 +2,17 @@ package demo1;
 
 public class DemoTest9 {
 	 public static void main (String args []) {
-		 System.out.println("JavaIsFun");
+		 System.out.println("MyKids");
+		 int a = 100;
+		 int b = 5;
+		 int result = a/b;
+		 
+	 
+	 System.out.println(result);
+	 }
+	 
+	 public void subtract(int a , int b) {
+		 System.out.println(a-b);
 	 }
 
 }
